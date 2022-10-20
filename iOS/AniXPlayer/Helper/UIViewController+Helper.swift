a51bf1cc257b2c4bb9cea088bf6d10a5d3a3d5f7
jimhuang <sun_8434@163.com>
2022-10-20 23:49:53 +0800

    [add]适配ipad

--- iOS/AniXPlayer/Helper/UIViewController+Helper.swift
+++ iOS/AniXPlayer/Helper/UIViewController+Helper.swift
@@ -9,14 +9,16 @@ import UIKit
 
 extension UIViewController {
     func present(_ viewControllerToPresent: UIViewController, atView view: UIView?, completion: (() -> Void)? = nil) {
-        if UIDevice.current.isPad {
+        if UIDevice.current.isPad && view != nil {
+            viewControllerToPresent.modalPresentationStyle = .popover
             viewControllerToPresent.popoverPresentationController?.sourceView = view
         }
         self.present(viewControllerToPresent, animated: true, completion: nil)
     }
     
     func present(_ viewControllerToPresent: UIViewController, atItem item: UIBarButtonItem?, completion: (() -> Void)? = nil) {
-        if UIDevice.current.isPad {
+        if UIDevice.current.isPad && item != nil {
+            viewControllerToPresent.modalPresentationStyle = .popover
             viewControllerToPresent.popoverPresentationController?.barButtonItem = item
         }
         self.present(viewControllerToPresent, animated: true, completion: nil)
