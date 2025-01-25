b77e9189b7abeb57ba7800bc016590fdad773272
jimhuang <jimhuang@futunn.com>
2025-01-25 21:31:38 +0800

    [fix]更新打包脚本

--- Mac/create-dmg.sh
+++ Mac/create-dmg.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-create-dmg ./AniXPlayer.app
+
+# 检查是否提供了参数
+if [ $# -lt 1 ]; then
+    echo "使用方法: $0 <文件路径>"
+    exit 1
+fi
+
+# 获取第一个参数（文件路径）
+file_path="$1"
+
+# 获取文件的目录
+dir_path=$(dirname "$file_path")
+
+# 切换到文件的目录
+cd "$dir_path" || { echo "无法切换到目录 '$dir_path'"; exit 3; }
+
+create-dmg $1 --identity="Apple Development: Hongjun Xu (LCV4UPFZ8P)"
