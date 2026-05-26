2c4ecff0f01fa6ee6eb2d22669f9a6e5ec1ecbe1
jimhuang <jimhuang@futunn.com>
2026-05-26 22:35:43 +0800

    chore(tvos): 添加 UIView+HUD 扩展和 TextField 基类

---
+++ tvOS/AniXPlayer/Vendor/Base/View/TextField.swift
@@ -0,0 +1,27 @@
+//
+//  TextField.swift
+//  AniXPlayer
+//
+//  tvOS TextField 基类
+//
+
+import UIKit
+
+class TextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setupInit()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.setupInit()
+    }
+
+    private func setupInit() {
+        self.font = .ddp_normal
+        self.textColor = .white
+        self.backgroundColor = .adaptiveSecondaryBackground
+    }
+}
